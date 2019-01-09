01ee8bfcce92d4e8b1b703f30a19f9ad1189b843
Kurt <mrkurt@gmail.com>
2019-01-09 13:16:58 -0600

    example ts

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-// stuff
+import { backends, middleware, pipeline } from "@fly/cdn";
+
+const mw = pipeline(
+  middleware.httpsUpgrader,
+  middleware.httpCache
+)
+
+const app = mw(
+  backends.origin("https://getting-started.edgeapp.net")
+);
+
+fly.http.respondWith(app);
